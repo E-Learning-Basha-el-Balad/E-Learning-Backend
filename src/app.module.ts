b041be00acc1146caa0d6a8a0f1aeaab3511fc77
Mostafa <mostafa.abuzahra@student.giu-uni.de>
2024-12-07 21:00:51 +0200

    Notes complete

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './users/users.module';
-import { CoursesModule } from './courses/courses.module';
-import { ModulesModule } from './modules/modules.module';
-import { QuizzesModule } from './quizzes/quizzes.module';
-import { ResponsesModule } from './responses/responses.module';
-import { ProgressModule } from './progress/progress.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [UserModule, CoursesModule, ModulesModule, QuizzesModule, ResponsesModule, ProgressModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/elearning'),
+    NotesModule,
+  ],
 })
 export class AppModule {}
