ce9afabe9b8c5b5ec5bd9842614fecb315574a1f
Abdulrahman Shaker <abdulrahmanshakeroff@gmail.com>
2024-12-24 20:04:51 +0200

    Fixed cors origin issue

--- src/main.ts
+++ src/main.ts
@@ -15,9 +15,9 @@ async function bootstrap() {
   app.use(cookieParser());
 
   
-  // app.enableCors({
-  //   origin: 'http://localhost:3001',
-  //   credentials: true});
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true});
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
